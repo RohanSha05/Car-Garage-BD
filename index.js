db2113fe26e510c7ae23b30645211b5ab5328e11
RohanSha05 <rhnshariar@gmail.com>
2022-05-08 22:39:56 +0600

    addNewItem

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 const app = express();
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.foqyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('carGarageBd').collection('Item');
+
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemCollection.findOne(query);
+            res.send(item);
+        })
+
+        app.post('/item', async (req, res) => {
+            const newItem = req.body;
+            console.log('adding new items', newItem)
+            const result = await itemCollection.insertOne(newItem)
+            res.send(result)
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.use(cors());
 app.use(express.json());
@@ -13,5 +51,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('Listening to port', port)
+    console.log('Listening to port', port);
 })
