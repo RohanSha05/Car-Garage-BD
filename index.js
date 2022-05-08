ddfb842a77cb7db11761a4fc45e794a42139ce00
RohanSha05 <rhnshariar@gmail.com>
2022-05-08 23:15:38 +0600

    deleteItems

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+// const ObjectId = require('mongodb').ObjectId; 
 
 const app = express();
 
@@ -34,6 +35,15 @@ async function run() {
             const result = await itemCollection.insertOne(newItem)
             res.send(result)
         });
+
+        //delete any item
+
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
